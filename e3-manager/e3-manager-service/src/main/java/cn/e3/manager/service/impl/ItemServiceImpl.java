dd3a893e35409e71a48e91bef5b02f4babf9e378
utopian <403584467@qq.com>
2017-12-29 21:01:03 +0800

    1th commit e3-manager

---
+++ e3-manager/e3-manager-service/src/main/java/cn/e3/manager/service/impl/ItemServiceImpl.java
@@ -0,0 +1,28 @@
+package cn.e3.manager.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cn.e3.manager.service.ItemService;
+import cn.e3.mapper.TbItemMapper;
+import cn.e3.pojo.TbItem;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+	@Autowired
+	private TbItemMapper tbItemMapper;
+	
+	/**
+	 * 需求:根据商品id查询商品
+	 * 参数:Long itemId
+	 * 返回值:TbItem
+	 */
+	@Override
+	public TbItem findItemById(Long itemId) {
+		//根据商品id查询商品
+		TbItem item = tbItemMapper.selectByPrimaryKey(itemId);
+		return item;
+	}
+
+}
